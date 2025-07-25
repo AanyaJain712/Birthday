34f5e9dc19683f73b8b49b8353a9c01cd9da2a8a
Aanya <aanyajain712@gmail.com>
2025-07-25 11:59:17 -0500

    Initial Commit

---
+++ Birthday/BirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayApp.swift
+//  Birthday
+//
+//  Created by Aanya Jain on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
